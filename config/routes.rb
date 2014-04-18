badbd9c6010b00bce76a44361edf9fc1a8d6c42f
Ryohei Fujita <ryohei@rento.jp>
2014-04-19 01:15:57 +0900

    Changed CSS

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Pinteresting2::Application.routes.draw do
+  root "pages#home"
+  get "about" => "pages#about" 
+  get "test" => "pages#test"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
