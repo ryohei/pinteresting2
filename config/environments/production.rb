c6a8af5a054e7f581a38b0a91cd3d971647aa96a
Ryohei Fujita <ryohei@rento.jp>
2014-04-19 16:57:52 +0900

    Add Devise

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,4 +77,7 @@ Pinteresting2::Application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
+
+  #Should chante to actual host name afterword.
+  config.action_mailer.default_url_options = { host: 'localhost:3000' }
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Pinteresting2::Application.routes.draw do
+  devise_for :users
   root "pages#home"
   get "about" => "pages#about" 
   get "test" => "pages#test"
